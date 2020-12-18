f09d2199ed8972a89941de95fc17c82b4351a58a
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-12-18 16:02:05 +0530

    Update sortquick_hoare.cpp

--- sortquick_hoare.cpp
+++ sortquick_hoare.cpp
@@ -23,23 +23,15 @@ int low(int a[],int l,int r)
 
 int high(int a[],int l,int r)
 {
-    a[l]=a[l]^a[r]^(a[r]=a[l]);
-    int pivot=a[r],i=l-1,j=r+1;
-    while(true)
+    int pivot=a[r],i=l,j=r;
+    while(i<j)
     {
-        do
-        {
-            i++;
-        }while(a[i]<pivot);
-
-        do
-        {
-            j--;
-        }while(a[j]>pivot);
-
-        if(i>=j) return i;
+        while(i<=r && a[i]<pivot) i++;
+        while(j>=0 && a[j]>pivot) j--;
+        if(i>=j) return i-1;
         a[i]=a[i]^a[j]^(a[j]=a[i]);
     }
+    return 0;   //this line is never reached
 }
 
 int mid(int a[],int l,int r)
@@ -66,8 +58,8 @@ void quicksort(int a[],int l,int r)
 {
     if(l<r)
     {
-        //int p=low(a,l,r);
-        int p=high(a,l,r);
+        int p=low(a,l,r);
+        //int p=high(a,l,r);
         //int p=mid(a,l,r);
         quicksort(a,l,p);
         quicksort(a,p+1,r);
